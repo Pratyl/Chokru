52f2c935a4d8ff8a2589df6e17641142eec353d4
Pratyl <93477420+Pratyl@users.noreply.github.com>
2025-05-29 09:34:13 +0530

    Add files via upload

---
+++ chokru/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 text-center py-10">
+      <h1 className="text-4xl font-bold mb-4">Welcome to Chokru!</h1>
+      <p className="text-lg">Enjoy jokes and shayari. More features coming soon.</p>
+    </div>
+  );
+}
+
+export default App;
